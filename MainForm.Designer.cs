a8a9f038fb3b23b3bf703102450c77dee0c0f52a
vlogShluhi <141409069+1Ibanok@users.noreply.github.com>
2025-05-25 20:11:44 +0300

    commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿using System.Drawing;
+
+namespace PR4_2
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            dataGridView1 = new DataGridView();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            saveBtn = new Button();
+            pictureBoxLogo = new PictureBox();
+            goods = new Button();
+            basket = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = Color.FromArgb(255, 255, 153);
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridView1.Location = new Point(10, 11);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 24;
+            dataGridView1.Size = new Size(919, 485);
+            dataGridView1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            comboBox1.BackColor = Color.FromArgb(255, 255, 153);
+            comboBox1.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "поставки", "список_поставки", "продажи", "сотрудники", "список_продажи", "товары" });
+            comboBox1.Location = new Point(939, 29);
+            comboBox1.Margin = new Padding(3, 2, 3, 2);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(248, 24);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(939, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(115, 16);
+            label1.TabIndex = 2;
+            label1.Text = "Выберите таблицу:";
+            label1.Click += label1_Click;
+            // 
+            // saveBtn
+            // 
+            saveBtn.BackColor = Color.FromArgb(57, 121, 61);
+            saveBtn.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            saveBtn.ForeColor = Color.White;
+            saveBtn.Location = new Point(939, 57);
+            saveBtn.Margin = new Padding(3, 2, 3, 2);
+            saveBtn.Name = "saveBtn";
+            saveBtn.Size = new Size(228, 24);
+            saveBtn.TabIndex = 3;
+            saveBtn.Text = "Сохранить";
+            saveBtn.UseVisualStyleBackColor = false;
+            saveBtn.Click += saveBtn_Click;
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.Image = (Image)resources.GetObject("pictureBoxLogo.Image");
+            pictureBoxLogo.Location = new Point(1078, 85);
+            pictureBoxLogo.Margin = new Padding(3, 2, 3, 2);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.Size = new Size(67, 63);
+            pictureBoxLogo.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxLogo.TabIndex = 5;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // goods
+            // 
+            goods.BackColor = Color.FromArgb(57, 121, 61);
+            goods.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            goods.ForeColor = Color.White;
+            goods.Location = new Point(939, 85);
+            goods.Margin = new Padding(3, 2, 3, 2);
+            goods.Name = "goods";
+            goods.Size = new Size(111, 36);
+            goods.TabIndex = 6;
+            goods.Text = "Товары";
+            goods.UseVisualStyleBackColor = false;
+            goods.Click += goods_Click;
+            // 
+            // basket
+            // 
+            basket.BackColor = Color.FromArgb(57, 121, 61);
+            basket.Font = new Font("Times New Roman", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            basket.ForeColor = Color.White;
+            basket.Location = new Point(939, 125);
+            basket.Margin = new Padding(3, 2, 3, 2);
+            basket.Name = "basket";
+            basket.Size = new Size(111, 35);
+            basket.TabIndex = 7;
+            basket.Text = "Корзина";
+            basket.UseVisualStyleBackColor = false;
+            basket.Click += basket_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1190, 507);
+            Controls.Add(basket);
+            Controls.Add(goods);
+            Controls.Add(pictureBoxLogo);
+            Controls.Add(saveBtn);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(dataGridView1);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "MainForm";
+            Text = "Главная";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.PictureBox pictureBoxLogo;
+        private Button goods;
+        private Button basket;
+    }
+}
